4158bd38a417a11f72105cd9d96fbf0c0b0e3def
Alex Law <pokemon999101@hotmail.co.uk>
2024-06-09 23:19:59 +0100

    🚛 refactorino

---
+++ AppParser.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const matter = require('gray-matter');
+
+let AppParser = (notesPath, ignoredDirectories) => {
+    if (ignoredDirectories === undefined) {
+        ignoredDirectories = ['.git', '.trash', '.obsidian', '.gitignore'];
+    }
+
+    let columns = new Set(['path', 'content']);
+
+    // Recursively read all files and directories apart from .git
+    const readDir = (dir) => {
+        let results = [];
+
+        const files = fs.readdirSync(dir);
+        for (const file of files) {
+            if (ignoredDirectories.includes(file)) {
+                continue;
+            };
+
+            let result = {};
+
+            const path = `${dir}/${file}`;
+            result.path = path;
+            if (fs.statSync(path).isDirectory()) {
+                // Merge results from the recursive call
+                results = results.concat(readDir(path));
+            } else {
+                // console.log(`🔎 Reading note: ${path}`);
+                const content = readNote(path);
+
+                if (!content) {
+                    result.content = 'Error reading note: Not a markdown file';
+                    results.push(result);
+                    continue;
+                }
+
+                result.content = content;  // Use result instead of results
+
+                // console.log(`Markdown file: ${path}`);
+
+                try {
+                    const note = matter(content);
+                    // console.log(note.data);
+                    // merge result with frontmatter
+                    result = { ...result, ...note.data };
+                    columns.add(...Object.keys(note.data));
+                } catch (e) {
+                    result.md_parse_error = 'Error reading frontmatter: ' + e;
+                    // console.log('Error reading note');
+                }
+
+                results.push(result);
+            }
+        }
+
+        return results;  // Ensure the accumulated results are returned
+    }
+
+    const readNote = (path) => {
+        // if its a text file, read it
+        if (!path.endsWith('.md')) {
+            return null;  // Ensure we return null for non-markdown files
+        }
+
+        const content = fs.readFileSync(path, 'utf8');
+
+        // stop execution of node totally
+        return content;
+    }
+
+    let rows = readDir(notesPath);
+
+    // console.log(`📝 Found ${rows.length} notes`);
+
+    return {
+        columns: Array.from(columns),
+        rows: rows
+    };
+}
+
+module.exports = AppParser;
--- index.js
+++ index.js
@@ -1,73 +1,12 @@
+const AppParser = require('./AppParser.js');
 const fs = require('fs');
-const ignoreDirectories = ['.git', '.trash', '.obsidian', '.gitignore'];
-const matter = require('gray-matter');
 const Papa = require('papaparse');
 
-const notesPath = '/home/cookie/projects/obsidian-notes';
-
-// Recursively read all files and directories apart from .git
-const readDir = (dir) => {
-    let results = [];
-
-    const files = fs.readdirSync(dir);
-    for (const file of files) {
-        if (ignoreDirectories.includes(file)) {
-            continue;
-        };
-
-        const result = {};
-
-        const path = `${dir}/${file}`;
-        result.path = path;
-        if (fs.statSync(path).isDirectory()) {
-            // Merge results from the recursive call
-            results = results.concat(readDir(path));
-        } else {
-            console.log(`🔎 Reading note: ${path}`);
-            const content = readNote(path);
-
-            if (!content) {
-                result.content = null;
-                results.push(result);
-                continue;
-            }
-
-            result.content = content;  // Use result instead of results
-
-            console.log(`Markdown file: ${path}`);
-
-            try {
-                const note = matter(content);
-                console.log(note.data);
-                result.frontmatter = note.data;  // Use result instead of results
-            } catch (e) {
-                result.frontmatter = null;
-                console.log('Error reading note');
-            }
-
-            results.push(result);
-        }
-    }
-
-    return results;  // Ensure the accumulated results are returned
-}
-
-const readNote = (path) => {
-    // if its a text file, read it
-    if (!path.endsWith('.md')) {
-        return null;  // Ensure we return null for non-markdown files
-    }
-
-    const content = fs.readFileSync(path, 'utf8');
-
-    // stop execution of node totally
-    return content;
-}
-
-let results2 = readDir(notesPath);
-
-console.log(`📝 Found ${results2.length} notes`);
+const results = AppParser('/home/cookie/projects/obsidian-notes', ['.git', '.obsidian', '.gitignore']);
 
 // Convert to CSV
-const csv = Papa.unparse(results2);
+const csv = Papa.unparse(results.rows, {
+    header: true,
+    columns: results.columns
+});
 fs.writeFileSync('storage/results.csv', csv);
